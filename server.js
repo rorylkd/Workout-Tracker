7dccf4c7e8eb0f0b9f9013d04ae86d31c9c5847a
rorylkd <rory.donaldson14@gmail.com>
2021-07-18 10:10:29 +0800

    Added htmlroutes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutTrackerDB", { useNewUrlParser: true });
 
